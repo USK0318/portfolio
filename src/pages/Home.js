1f613d8edae4fe5ae3863f1d93e0d826fc4562fb
usk0318 <saikiranuppalapati2@gmail.com>
2024-12-25 16:18:42 +0530

    fix: update CV download links in Home.js for correct file access

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -72,10 +72,10 @@ const GetCVButton = styled(Button)({
 
 const downloadCV = () => {
   // Change 'path_to_cv.pdf' to the actual path of your CV
-  const cvPath = 'https://drive.google.com/u/0/uc?id=1AoN2aJdihPg2ZL3w8lkw6sEIox7r8Das&export=download';
+  const cvPath = 'https://drive.google.com/uc?export=download&id=1cEstpyaBXnRCnEp5-PGLRPI4qPNxPa-e';
   const link = document.createElement('a');
   link.href = cvPath;
-  link.download = 'https://drive.google.com/file/d/1AoN2aJdihPg2ZL3w8lkw6sEIox7r8Das/view'; // You can change the downloaded file name
+  link.download = 'https://drive.google.com/uc?export=download&id=1cEstpyaBXnRCnEp5-PGLRPI4qPNxPa-e'; // You can change the downloaded file name
   document.body.appendChild(link);
   link.click();
   document.body.removeChild(link);
