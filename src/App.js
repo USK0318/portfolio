358f554e5a12cd4d9b465c4185474d3af80b4098
usk0318 <saikiranuppalapati2@gmail.com>
2024-12-25 16:11:12 +0530

    feat: add loading screen component with animation and integrate into App

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Header from './comp/Header';
@@ -7,19 +7,31 @@ import Contact from './pages/Contact';
 import Project from './pages/Project';
 import Home from './pages/Home';
 import { Footer } from './comp/Footer';
+import Loader from './Loader'; // Import the loader
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setLoading(false), 4000);
+    return () => clearTimeout(timer); 
+  }, []);
+
+  if (loading) {
+    return <Loader />;
+  }
+
   return (
     <>
-      <div ><Header /></div>
-      <div class ='final'>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/Contact" element={<Contact />} />
-        <Route path="/Projects" element={<Project />} />
-        <Route path="/About" element={<About />} />
-      </Routes>
-      <Footer/>
+      <div><Header /></div>
+      <div className="final">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Contact" element={<Contact />} />
+          <Route path="/Projects" element={<Project />} />
+          <Route path="/About" element={<About />} />
+        </Routes>
+        <Footer />
       </div>
     </>
   );
---
+++ src/Loader.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Loader.css';
+
+const Loader = () => {
+  return (
+    <div className="loader-container">
+      <div className="loader">Hi, this is Uppalapati Sai Kiran</div>
+      <div className="subtext">A Full Stack Developer</div>
+    </div>
+  );
+};
+
+export default Loader;
